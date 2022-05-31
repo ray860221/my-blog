837dbef9eda78336b520ed939e67df3979382b77
viplab <viplab@DESKTOP-R083HCS>
2022-05-31 18:34:49 +0800

    feat: header done

--- components/Header.js
+++ components/Header.js
@@ -1,46 +1,55 @@
 import styled from 'styled-components';
-import Link from 'next/link';
 
 const Wrapper = styled.div`
-  background-color: #cdcdcd;
+  background-color: #d0dded;
 `;
 const NavBar = styled.div`
   display: flex;
   align-items: center;
+  height: 120px;
   max-width: 800px;
   margin: 0 auto;
-  padding: 24px 0;
 `;
 const NavTitle = styled.div`
   font-size: 32px;
+  letter-spacing: 4px;
   margin-right: 40px;
+  cursor: pointer;
 `;
 
 const NavItem = styled.div`
   font-size: 16px;
   margin-right: 40px;
   cursor: pointer;
-`;
-const Title = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  font-size: 60px;
-  padding: 48px 0 120px 0;
+  transition: all 0.3s;
+  border-bottom: 1px solid transparent;
+  &:hover {
+    border-bottom: 1px solid;
+  }
 `;
 
-const Header = () => {
+const PageTitle = styled.div`
+  padding: 20px 0 100px 0;
+  max-width: 800px;
+  margin: 0 auto;
+  font-size: 50px;
+  font-weight: bold;
+  letter-spacing: 5px;
+`;
+
+const Header = ({ navItems, nowPage }) => {
   return (
     <Wrapper>
       <NavBar>
         <NavTitle>Ray's Blog</NavTitle>
-        <NavItem>Home</NavItem>
-        <NavItem>Post</NavItem>
-        <NavItem>About</NavItem>
+        {navItems.map((item) => (
+          <NavItem key={item}>{item}</NavItem>
+        ))}
         {/* <Link href={'/posts'}>
                     <NavItem>Post</NavItem>
                 </Link> */}
       </NavBar>
-      <Title>Home</Title>
+      <PageTitle>{nowPage}</PageTitle>
     </Wrapper>
   );
 };
