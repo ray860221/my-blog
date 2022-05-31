4db8982143d2f5207a38454ad4931aeffc94c44f
viplab <viplab@DESKTOP-R083HCS>
2022-05-31 12:09:25 +0800

    feat: add header component(undone)

---
+++ components/Header.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+import Link from 'next/link'
+
+const Wrapper = styled.div`
+    background-color: #cdcdcd;
+`
+const NavBar = styled.div`
+    display: flex;
+    align-items: center;
+	max-width: 800px;
+    margin: 0 auto;
+    padding: 24px 0;
+`
+const NavTitle = styled.div`
+    font-size: 32px;
+    margin-right: 40px;
+`
+
+const NavItem = styled.div`
+    font-size: 16px;
+    margin-right: 40px;
+    cursor: pointer;
+`
+const Title = styled.div`
+	max-width: 800px;
+    margin: 0 auto;
+    font-size: 60px;
+    padding: 48px 0 120px 0;
+`
+
+const Header = () => {
+
+  	return (
+    	<Wrapper>
+            <NavBar>
+                <NavTitle>Ray's Blog</NavTitle>
+                <NavItem>Home</NavItem>
+                <NavItem>Post</NavItem>
+                <NavItem>About</NavItem>
+                {/* <Link href={'/posts'}>
+                    <NavItem>Post</NavItem>
+                </Link> */}
+            </NavBar>
+            <Title>Home</Title>
+    	</Wrapper>
+  	)
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,39 @@
+import styled from 'styled-components'
+import Header from '../components/Header'
 import Link from 'next/link'
+import { getAllPostData  } from '../src/posts';
 
-const Home = () => {
+
+export async function getStaticProps() {
+	const postData = await getAllPostData();
+	return {
+		props:  {
+			postData,
+		},
+	};
+}
+
+const Wrapper = styled.div``
+const Content = styled.div`
+	max-width: 800px;
+	margin: 0 auto;
+`
+
+const Home = ({ postData }) => {
   	return (
-    	<>
-			<h1>Home</h1>
-			<Link href={'/posts'}>
-				<a><h1>Post</h1></a>
-			</Link>
-    	</>
+    	<Wrapper>
+			<Header />
+			<Content>
+				{postData.map((post) => (
+					<div key={post.id}>
+						<div>
+							<h1 style={{'cursor': 'pointer'}}>{post.title}</h1>
+							<p>{post.date}</p>
+						</div>
+					</div>
+				))}
+			</Content>
+    	</Wrapper>
   	)
 }
 
